13821db27a340d9a62a077dc250642e1747d1b51
CHT7 <caohoangtancht@gmail.com>
2025-08-17 00:12:56 +0700

    Add Find The Differences script

---
+++ scripts/games/Find The Differences/main.lua
@@ -0,0 +1,9 @@
+local Stages = game:GetService("ReplicatedStorage").Stages:GetChildren()
+table.sort(Stages, function(a,b)
+    return tonumber(a.Name) < tonumber(b.Name)
+end)
+for _,stage in pairs(Stages) do
+    game:GetService("ReplicatedStorage").Modules.Net:FindFirstChild("RE/StageComplete"):FireServer(stage)
+    print("Đã hoàn thành màn:",stage.Name)
+    wait(0.2)
+end
