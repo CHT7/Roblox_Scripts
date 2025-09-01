86a172558e28318cfeeb40d7daaa8027af2c9b7a
CHT7 <caohoangtancht@gmail.com>
2025-09-01 14:30:15 +0700

    fix complete stage

--- scripts/games/Find The Differences/main.lua
+++ scripts/games/Find The Differences/main.lua
@@ -3,7 +3,7 @@ table.sort(Stages, function(a,b)
     return tonumber(a.Name) < tonumber(b.Name)
 end)
 for _,stage in pairs(Stages) do
-    game:GetService("ReplicatedStorage").Modules.Net:FindFirstChild("RE/StageComplete"):FireServer(stage)
+    game:GetService("ReplicatedStorage").Modules.Net:FindFirstChild("RE/StageComplete"):FireServer(tonumber(stage.Name))
     print("Đã hoàn thành màn:",stage.Name)
     wait(0.2)
 end
